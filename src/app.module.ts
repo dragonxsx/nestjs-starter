c29945942e619560087d00e4f914ef9648355bf5
Nguyen Thanh Long <thanhlong123_vietnam@yahoo.com.vn>
2020-08-18 17:07:03 +0700

    Add config loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsController } from './posts/posts.controller';
-import { PostsService } from './posts/posts.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number()
+        })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
