ac651c73228f0151514f0e9103e71469bc29a2c0
Nguyen Thanh Long <dragon.sunshine@gmail.com>
2020-09-09 12:09:00 +0700

    Create Many-To-Many relations

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Transform } from 'class-transformer';
 import User from '../users/user.entity';
+import Category from '../categories/category.entity';
 
 
 @Entity()
@@ -24,6 +25,10 @@ class Post {
 
   @ManyToOne(() => User, (author: User) => author.posts)
   public author: User;
+
+  @ManyToMany(() => Category, (category: Category) => category.posts)
+  @JoinTable()
+  public categories: Category[];
 }
 
 export default Post;
